272678181de384796882eafbdf1db46c202847d0
Juri Pakaste <juri@iki.fi>
2016-03-22 18:40:07 +0200

    Remove targets declaration from Package.swift

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "Flue",
-    targets: [
-        Target(name: "Flue"),
-        Target(name: "FlueTests", dependencies: [.Target(name: "Flue")])
-    ]
+    // targets: [
+    //     Target(name: "Flue"),
+    //     Target(name: "FlueTests", dependencies: [.Target(name: "Flue")])
+    // ]
 )
